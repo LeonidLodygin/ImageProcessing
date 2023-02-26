eccfb99e7d5d3f0cdfcffb2db15ea8388dbeb071
LeonidLodygin <st108157@student.spbu.ru>
2023-02-26 15:20:45 +0300

    Changed the image array processing function

--- src/ImageProcessing/ImageArrayProcessing.fs
+++ src/ImageProcessing/ImageArrayProcessing.fs
@@ -16,23 +16,17 @@ let Extensions =
 
 let listAllFiles dir =
     let files = System.IO.Directory.GetFiles dir
-    printfn $"%A{files}"
     // Filter the obtained files by extension
     let filtered =
         Array.filter (fun (x: string) -> Array.contains (x.Substring(x.LastIndexOf('.'))) Extensions) files
 
-    printfn $"%A{filtered}"
     List.ofArray filtered
 
 let ArrayOfImagesProcessing inputDir outputDir conversion =
     let list = listAllFiles inputDir
 
-    let rec helper lst =
-        match lst with
-        | [] -> ()
-        | hd :: tl ->
-            let filtered = conversion (loadAs2DArray hd)
-            save2DByteArrayAsImage filtered (System.IO.Path.Combine(outputDir, System.IO.Path.GetFileName hd))
-            helper tl
+    let helper filePath =
+        let filtered = conversion (loadAs2DArray filePath)
+        save2DByteArrayAsImage filtered (System.IO.Path.Combine(outputDir, System.IO.Path.GetFileName filePath))
 
-    helper list
+    List.iter helper list
